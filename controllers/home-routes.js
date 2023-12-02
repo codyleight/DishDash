e1c7ca17ceceaabf35649e42264aafea9a519dd0
Cody Thompson <codylthomp@gmail.com>
2023-12-02 12:32:33 -0600

    'added Resturants and reviews api, and route handlers, Added resturants a nd reviews.handlebars files with basic structure of what we will need in the fut ure'

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -22,4 +22,27 @@ router.get('/login', (req, res) => {
   res.render('login');
 });
 
+router.get('/reviews', (req, res) => {
+  try {
+    res.render('reviews', {
+    loggedIn: req.session.loggedIn,
+  });
+} catch (err) {
+  console.log(err);
+  res.status(500).json(err);
+}
+});
+
+
+router.get('/resturants', (req, res) => {
+  try {
+    res.render('resturants', {
+    loggedIn: req.session.loggedIn,
+  });
+} catch (err) {
+  console.log(err);
+  res.status(500).json(err);
+}
+});
+
 module.exports = router;
